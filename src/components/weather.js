925526876b54d2cff1a82a58337bbfee7b4bbc27
JT Taylor <8219785+josetaylor06@users.noreply.github.com>
2024-03-02 19:15:54 -0600

    added react

---
+++ src/components/weather.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../index.css';
+import { Card } from 'semantic-ui-react'
+import moment from 'moment';
+
+
+const CardExampleCard = ({weatherData}) => (
+    <Card>
+      <Card.Content>
+          <Card.Header className="header">Your City Name: {weatherData.name}</Card.Header>
+          <p>Day: {moment().format('dddd')}</p>
+          <p>Date: {moment().format('LL')}</p>
+          <p>Temprature: {weatherData.main.temp} &deg;F</p>
+          <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+          <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+          <p>Description: {weatherData.weather[0].main}</p>
+          <p>Humidity: {weatherData.main.humidity} %</p>
+      </Card.Content>
+    </Card>
+  )
+  
+  export default CardExampleCard;
