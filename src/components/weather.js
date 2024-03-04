2c2d43c66b9119a4b801454faaff66e3240c6cdc
JT Taylor <8219785+josetaylor06@users.noreply.github.com>
2024-03-03 22:43:40 -0600

    Updated date line, removed day paragraph

--- src/components/weather.js
+++ src/components/weather.js
@@ -8,8 +8,7 @@ const CardExampleCard = ({weatherData}) => (
     <Card>
       <Card.Content>
           <Card.Header className="header">Your City Name: {weatherData.name}</Card.Header>
-          <p>Day: {moment().format('dddd')}</p>
-          <p>Date: {moment().format('LL')}</p>
+          <p>{moment().format('dddd')}: {moment().format('LL')}</p>
           <p>Temprature: {weatherData.main.temp} &deg;F</p>
           <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
           <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
